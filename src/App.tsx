a925b004f1eb550fecb8abae4193b8c8bd5e73d3
임성훈 <limpanda7@naver.com>
2023-01-18 00:00:31 +0900

    web3modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+import {Web3Modal, useWeb3Modal} from "@web3modal/react";
+import {configureChains, createClient, WagmiConfig} from "wagmi";
+import {mainnet} from "wagmi/chains";
 
-function App() {
+const App = () => {
+
+  const chains = [mainnet];
+
+  // Wagmi client
+  const {provider} = configureChains(chains, [
+    walletConnectProvider({projectId: 'dccff4a005c3731fbd4bce50f884a8ad'}),
+  ]);
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors: modalConnectors({appName: "web3Modal", chains}),
+    provider,
+  });
+
+  // Web3Modal Ethereum Client
+  const ethereumClient = new EthereumClient(wagmiClient, chains);
+
+  const {open} = useWeb3Modal();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <>
+          <button onClick={() => open()}>지갑연결</button>
+        </>
+      </WagmiConfig>
+
+      <Web3Modal
+        projectId="dccff4a005c3731fbd4bce50f884a8ad"
+        ethereumClient={ethereumClient}
+      />
+    </>
   );
 }
 
