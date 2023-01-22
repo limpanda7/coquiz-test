6604f14a8e3824ea4d3b91f0b8a1cd7cb1a53542
임성훈 <limpanda7@naver.com>
2023-01-22 21:48:29 +0900

    params -> query string 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Modal from "./Modal";
+import React, {useEffect, useState} from "react";
+import {mainnet} from "wagmi/chains";
+import {configureChains, createClient, useAccount, WagmiConfig} from "wagmi";
+import {EthereumClient, modalConnectors, walletConnectProvider} from "@web3modal/ethereum";
+import {useWeb3Modal, Web3Modal} from "@web3modal/react";
 import {initializeApp} from "firebase/app";
+import {collection, doc, getFirestore, setDoc} from "firebase/firestore";
+import queryString from "query-string";
 
 const firebaseConfig = {
   apiKey: "AIzaSyASfhe2jcjVIjSK89sIDRwFHzYIBNLqnlU",
@@ -11,15 +16,73 @@ const firebaseConfig = {
   appId: "1:455002882220:web:c30c8ea7baf200e5969f09"
 };
 
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 const App = () => {
+  const [firstOpen, setFirstOpen] = useState(true);
+
+  const chains = [mainnet];
+
+  // Wagmi client
+  const {provider} = configureChains(chains, [
+    walletConnectProvider({projectId: 'dccff4a005c3731fbd4bce50f884a8ad'}),
+  ]);
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors: modalConnectors({appName: "web3Modal", chains}),
+    provider,
+  });
+
+  // Web3Modal Ethereum Client
+  const ethereumClient = new EthereumClient(wagmiClient, chains);
+
+  const {open, isOpen} = useWeb3Modal();
+
+  const {address} = useAccount();
+
+  useEffect(() => {
+    setTimeout(() => {
+      open();
+      setFirstOpen(false);
+    }, 500);
+  }, []);
+
+  const params = window.location.search;
+  const {uid} = queryString.parse(params);
+  console.log(uid);
+
+  const saveAddress = () => {
+    const db = getFirestore(app);
+    const collectionRef = collection(db, 'user_wallet_address');
+    setDoc(doc(collectionRef, uid as string), {
+      wallet_address: address || ''
+    })
+  }
+
+  useEffect(() => {
+    if (!firstOpen) {
+      saveAddress();
+    }
+  }, [address]);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='modal/:uid' element={<Modal />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <>
+          {
+            (!isOpen && !firstOpen) &&
+            <div style={{textAlign: 'center'}}>
+              <h1>Please go back to COQUIZ APP</h1>
+            </div>
+          }
+        </>
+      </WagmiConfig>
+
+      <Web3Modal
+        projectId="dccff4a005c3731fbd4bce50f884a8ad"
+        ethereumClient={ethereumClient}
+      />
+    </>
   );
 }
 
